50bc42895bcfa89e27176ae11b3c3c33c7a9b221
Cesar Santos <csar.santos18@gmail.com>
2025-03-24 10:04:48 -0500

    API, password-reset and email-confirmation

--- accounts/utils.py
+++ accounts/utils.py
@@ -35,3 +35,32 @@ def send_activation_email(user):
     recipient_list = [user.email]
 
     send_mail(subject, message, from_email, recipient_list)
+
+
+def generate_reset_token(user):
+
+    data = {"user_id": user.id}
+    return signing.dumps(data)
+
+def decode_reset_token(token):
+
+    try:
+        data = signing.loads(token)
+        return data.get("user_id")
+    except signing.BadSignature:
+        raise ValueError("Token inválido")
+
+def send_reset_password_email(user):
+
+    token = generate_reset_token(user)
+    reset_link = f"http://127.0.0.1:8000/api/accounts/reset-password/{token}/"
+
+    subject = "Redefinição de senha"
+    message = (
+        f"Olá {user.first_name},\n\n"
+        f"Clique no link abaixo para redefinir sua senha:\n{reset_link}"
+    )
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [user.email]
+
+    send_mail(subject, message, from_email, recipient_list)
