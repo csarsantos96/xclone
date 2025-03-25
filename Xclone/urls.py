0560b2605cc936b99e7f0e068e06c443731db9f5
Cesar Santos <csar.santos18@gmail.com>
2025-03-25 10:37:04 -0500

    feat: add like, repost and reply

--- Xclone/urls.py
+++ Xclone/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from tweets.views import TweetListCreateAPIView, FeedTweetListAPIView, TweetDetailAPIView
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,9 +32,14 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/tweets/', include('tweets.urls')),
 
+    path('tweets/', TweetListCreateAPIView.as_view(), name='tweet-list-create'),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+path('', TweetListCreateAPIView.as_view(), name='tweet-list-create'),  # Para a criação de tweets
+    path('feed/', FeedTweetListAPIView.as_view(), name='feed-tweets'),  # Feed de tweets
+    path('<int:pk>/', TweetDetailAPIView.as_view(), name='tweet-detail'),  # Detalhes do tweet
 ]
 
 if settings.DEBUG:
