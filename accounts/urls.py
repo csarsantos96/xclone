100538ea3bbe7b031b35ed92ef0fa00f6ccc648a
Cesar Santos <csar.santos18@gmail.com>
2025-03-25 00:43:17 -0500

    feat: alterando o back end para receber o front

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,16 @@
-# accounts/urls.py
-
 from django.urls import path
 from .views import (
     UserRegisterAPIView,
     UserActivateAPIView,
     ForgotPasswordAPIView,
     ResetPasswordAPIView,
+    CreateUserAPIView,
 )
 
 urlpatterns = [
     path('register/', UserRegisterAPIView.as_view(), name='user-register'),
     path('activate/<path:token>/', UserActivateAPIView.as_view(), name='user-activate'),
-
-# Esqueci a senha
-
     path('forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot-password'),
     path('reset-password/<path:token>/', ResetPasswordAPIView.as_view(), name='reset-password'),
+    path('createUser/', CreateUserAPIView.as_view(), name='create_user'),
 ]
