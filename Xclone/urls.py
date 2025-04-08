59005935bca38d3421c0fab0b30960a955273c2e
Cesar Santos <csar.santos18@gmail.com>
2025-04-07 21:08:33 -0500

    Renomear pasta do projeto para xclone (minúsculo)

--- Xclone/urls.py
+++ Xclone/urls.py
@@ -1,6 +1,6 @@
 #root/urls
 """
-URL configuration for xclone project.
+URL configuration for Xclone project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
